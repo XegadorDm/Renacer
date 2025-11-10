f3e07299dfd92b2f1bfc017e98673c4b00c7c5f5
DIANA ZASALAR <dianazasalar1@gmail.com>
2025-11-10 05:53:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9557272207-8cb4a",
+  "appId": "1:979441687669:web:3cce16cac15cb985a4ac3c",
+  "apiKey": "AIzaSyBo8KQP8J4Mrpn-UCtCfJeYoZCqCTGcNcg",
+  "authDomain": "studio-9557272207-8cb4a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "979441687669"
+};
